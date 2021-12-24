2c50d9cb3c1c37eb18b4483dbbea77daa84b3abd
QiuQiu <qq982516861@gmail.com>
2021-12-24 09:13:25 +0800

    Update

--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -1,18 +1,24 @@
 import ReactMarkdown from 'react-markdown';
 import md from './demo.md?raw';
-import styles from './index.module.less';
+import './index.less';
 
 export default () => {
 	console.log(md);
 	return (
-		<div className={styles['blog-warp']}>
-			<div className={styles['container']}>
-				<ReactMarkdown children={md} />
+		<div className="blog-warp">
+			<div className="container-warp">
+				<div className="container">
+					<h1 className="title">标题</h1>
+					<div style={{ margin: '40px 0' }}>用户信息</div>
+					<ReactMarkdown children={md} />
+				</div>
+				<div className="comment-area">评论区</div>
 			</div>
-			<div className={styles['sidebar']}>
+
+			<div className="sidebar">
 				<div>作者</div>
 				<div>文章标签</div>
-				<div className={styles['directory-column']}>目录</div>
+				<div className="directory-column">目录</div>
 			</div>
 		</div>
 	);
