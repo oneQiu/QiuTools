36fb94e0f153d61d1487b08e7e1bb445487f7a88
QiuQiu <50658231+oneQiu@users.noreply.github.com>
2022-01-24 14:26:52 +0800

    Update CompLazy.tsx

--- src/components/TabDrawer/widget/CompLazy.tsx
+++ src/components/TabDrawer/widget/CompLazy.tsx
@@ -6,16 +6,19 @@ import { ReactNode, useEffect, useState } from 'react';
 interface IProps {
   children?: ReactNode;
   visible?: boolean;
+  initSetKey?: (handle: () => void) => void;
 }
-export default ({ children, visible }: IProps) => {
+export default ({ children, visible, initSetKey }: IProps) => {
+  const [key, setKey] = useState<number>(Math.random());
   const [showChild, setShowChild] = useState<boolean>(false);
 
   useEffect(() => {
+    initSetKey?.(() => setKey(Math.random()));
     if (visible && !showChild) setShowChild(true);
   });
 
   return (
-    <div style={{ display: visible ? 'block' : 'none' }}>
+    <div style={{ display: visible ? 'block' : 'none' }} key={key}>
       {showChild ? children : null}
     </div>
   );
