5374c2deed8a9c34a896ffd9b19c8e21f5496c6f
QiuQiu <50658231+oneQiu@users.noreply.github.com>
2023-06-27 07:43:01 +0000

    Update 去除打包hash

--- vite.config.ts
+++ vite.config.ts
@@ -4,23 +4,32 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]',
+      },
+    },
+  },
   resolve: {
     alias: {
-      '@': '/src'
-    }
+      '@': '/src',
+    },
   },
   server: {
     port: 4000,
-    open: true
+    open: true,
   },
   css: {
     modules: {
       scopeBehaviour: 'local',
       localsConvention: 'camelCase',
-      generateScopedName: '[name]_[local]_[hash:4]'
+      generateScopedName: '[name]_[local]_[hash:4]',
     },
     preprocessorOptions: {
-      less: true
-    }
-  }
+      less: true,
+    },
+  },
 });
